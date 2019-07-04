43580b3c2f6155d371bfa8bf21b50ebe5b86be7c
seong-hui <52481403+seong-hui@users.noreply.github.com>
2019-07-04 17:25:27 +0900

    중간 저장

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/mainMenu', function(req, res) {
+  res.render('mainMenu', { title: 'Express' });
+});
+
+
 module.exports = router;
 
 //지호
