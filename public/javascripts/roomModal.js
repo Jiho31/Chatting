1543d3812dfb8227efebc02c593c61ee4548e966
seong-hui <52481403+seong-hui@users.noreply.github.com>
2019-07-09 17:59:32 +0900

    making room 모달 디자인 완성
    데이터 연동 아직 안 됨

--- public/javascripts/roomModal.js
+++ public/javascripts/roomModal.js
@@ -4,7 +4,15 @@ var modal = document.getElementById('room_modal');
 var btn = document.getElementById("room_btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("room-close")[0];                                          
+var span = document.getElementsByClassName("room-close")[0];  
+
+//submit 버튼
+var submit_btn = document.getElementsByClassName("ui secondary submit button")[0];
+
+//cancel 버튼
+var cancel_btn = document.getElementsByClassName("ui grey button")[0];
+
+
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
@@ -22,6 +30,26 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+//채팅 방식 선택할 때 뒤에 네모 박스
+$(document).ready(function () {
+    $('.button.selector').on('click', function() {
+        $('.button.selector').removeClass('selected');
+        $(this).addClass('selected');
+    });});
+
+// 카테고리 선택지 나열
 $('.ui.dropdown')
   .dropdown()
 ;
+
+//submit 버튼 누르면 모달 닫힘
+submit_btn.onclick = function() {
+    modal.style.display = "none";
+}
+
+//cancel 버튼 누르면 모달 닫힘
+cancel_btn.onclick = function() {
+    modal.style.display = "none";
+}
+
