84ae3dc868a912e5801776aa3af2d412a169c9be
tjspeit <51429922+tjspeit@users.noreply.github.com>
2019-07-23 14:36:07 +0900

    kakao

--- public/javascripts/chattingList.js
+++ public/javascripts/chattingList.js
@@ -1,8 +1,66 @@
 $('.ui.dropdown').dropdown('');
 
+/*상단바 왼쪽*/
 $(document).ready(function () {
     $('.ui .item').on('click', function() {
         $('.ui .item').removeClass('active');
         $(this).addClass('active');
     });          
 });
+
+/*방만들기 모달*/
+var modal = document.getElementById('room_modal');
+ 
+// Get the button that opens the modal
+var btn = document.getElementById("room_btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("room-close")[0];  
+
+//submit 버튼
+var submitBtn = document.getElementsByClassName("ui secondary submit button")[0];
+
+//cancel 버튼
+var cancelBtn = document.getElementsByClassName("ui grey button")[0];
+
+
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//채팅 방식 선택할 때 뒤에 네모 박스
+$(document).ready(function () {
+    $('.button.selector').on('click', function() {
+        $('.button.selector').removeClass('selected');
+        $(this).addClass('selected');
+    });});
+
+// 카테고리 선택지 나열
+$('.ui.dropdown')
+  .dropdown()
+;
+
+//submit 버튼 누르면 모달 닫힘
+submitBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+//cancel 버튼 누르면 모달 닫힘
+cancelBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,30 @@ var express = require('express');
 var session = require('express-session');
 var mysql = require('mysql');
 var router = express.Router();
+var passport = require('passport');
+var KakaoStrategy = require('passport-kakao').Strategy;
 
-router.use(session({
-  secret: '1234DSFS@!',
-  resave: false,
-  saveUninitialized: true
-}));
+// localhost:3000/login/kakao로 들어오면(get으로 들어오면) passport.authenticate를 실행(여기서는 임의로 login-kakao로 이름을 줌)
+router.use(passport.initialize());
 
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'nodejs',
-  password: 'seonhye6166',
-  database: 'chatting'
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-connection.connect();
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+// 이름을 login-kakao로 임의로 주었습니다 그래서 /kakao로 들어오면 아래가 실행이 됩니다
+passport.use('login-kakao', new KakaoStrategy({
+        clientID : '093de487936421a33333e63ed00845b8',
+        callbackURL : 'http://randomchatting.cf:3000/oauth' // 카카오 개발자 사이트에서 지정한 리다이렉트 URL 
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        return done(null, profile);
+    }
+));
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -56,6 +65,38 @@ router.post('/new_qna', function(req, res){
   console.log(qnaContent);
   console.log(qnaCategory);
 });
+
+router.get('/login', passport.authenticate('login-kakao'));
+
+router.get('/oauth', passport.authenticate('login-kakao', {
+    successRedirect: '/chattingList', // 성공하면 /main으로 가도록
+    failureRedirect: '/'
+}));
+
+router.get('/maketable',function(req, res){
+  var sql = [];
+  
+  sql[0] = `
+    CREATE TABLE IF NOT EXISTS userInfo{
+      id VARCHAR(10),
+      grade INT, 
+      report INT,
+      list INT
+    }
+  `
+  
+  sql[1] = `
+    CREATE TABLE IF NOT EXISTS roomInfo{
+      roomNo INT NOT NULL,
+      category VARCHAR(10), 
+      roomName VARCHAR(10),
+      mode INT,
+      participation INT
+    }
+  `
+
+});
+
 module.exports = router;
 
 //지호
