ff3ee3ce7794215d79bf41f60295abcc24779a40
Ji Ho Bok <bok.jiho@gmail.com>
2019-07-24 20:41:03 +0900

    q&a 페이지 데이터베이스 연결
    - QNALIST 테이블 생성
    - 체크박스, 아이디 항목 제외하고 나머지 폼 데이터 테이블에 등록되는 데까지 완료

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 exports.getDBInfo = function() {
     var data = {
-        host:'localhost',
         user:'jiho',
         password:'12345678',
         database:'chatproject',
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('sync-mysql');
+var dbinfo = require('../database.js');
+var multer = require('multer');
+var path = require('path');
+var upload = multer({
+  //storage: multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     cb(null, 'uploads/');
+  //   },
+  //   filename: function (req, file, cb) {
+  //     cb(null, new Date().valueOf() + path.extname(file.originalname));
+  //   }
+  // }),
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, new Date().valueOf() + file.originalname); // 타임스탬프 + 원래파일명으로 저장 (파일명이 중복되는 경우를 위해)
+    }
+  }),
+  });
+
+var connectDB = new mysql(dbinfo.getDBInfo());
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -22,18 +46,49 @@ router.get('/qna', function(req, res) {
 });
 
 router.get('/writepost', function(req, res) {
+  connectDB.query("CREATE TABLE IF NOT EXISTS QNALIST(`index` INT NOT NULL AUTO_INCREMENT, userId CHAR(30), category TEXT, title TEXT, content TEXT, secretOrNot BOOLEAN, filePaths TEXT, date TEXT, PRIMARY KEY(`index`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
+
   res.render('writepost');
 });
 
-router.post('/new_qna', function(req, res){
+router.post('/new_qna1', function(req, res){
   var qnaTitle=req.body.qnaTitle;
   var qnaCategory=req.body.qnaCategory;
   var qnaContent=req.body.qnaContent;
+  //var qnaSecret=req.body.qnaSecret;
+  var qnaSecret;
+  var num=1;
 
-  console.log(qnaTitle);
-  console.log(qnaContent);
-  console.log(qnaCategory);
+  //userId 로그인 기능 추가하고 나중에 테이블에 저장
+  // secretOrNot 보류
+  var addPost = "UPDATE QNALIST SET category='"+qnaCategory+"', title='"+qnaTitle+"', content='"+qnaContent+"' WHERE QNALIST.index="+num+";";
+  connectDB.query(addPost);
+
+  // console.log(qnaTitle);
+  // console.log(qnaContent);
+  // console.log(qnaCategory);
+  //console.log(qnaSecret);
+
+  res.redirect('/qna');
 });
+
+router.post('/new_qna2', upload.array('img', 5), function(req, res){
+  var postDate = new Date();
+  var len = req.files.length; // 등록한 첨부 파일 갯수
+  var filePaths;
+  if(len>0){
+    filePaths = req.files[0].path; 
+    for(i=1; i<len; i++){
+      filePaths=filePaths+', '+req.files[i].path;
+    }
+  }
+  
+  var addPost = "INSERT INTO QNALIST(filePaths, date)";
+  addPost = addPost + "VALUES('"+ filePaths+"', '"+postDate+"');";
+  connectDB.query(addPost);
+  
+  res.send(true);
+})
 module.exports = router;
 
 //지호
