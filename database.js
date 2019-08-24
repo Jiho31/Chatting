201a02d525ed9c1aa432c0f2f4c11d1041577ec1
Ji Ho Bok <bok.jiho@gmail.com>
2019-08-24 20:41:22 +0900

    fixed untracked files

--- database.js
+++
@@ -1,9 +0,0 @@
-exports.getDBInfo = function() {
-    var data = {
-        user:'MOON',
-        password:'20000429',
-        database:'PROJECT1',
-        multipleStatements:true
-    }
-    return data;
-}
